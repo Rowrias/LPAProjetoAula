a09df6554c488fdc457afb4279abcedfb4720cb0
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-06-25 21:34:00 -0300

    commit antes de começar a codificar o Level

--- MountainShooter/code/Level.py
+++ MountainShooter/code/Level.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from code.Entity import Entity
+
 
 class Level:
-    def __init__(self):
-        self.window = None
-        self.entity_list = None
-        self.name = None
+    def __init__(self, window, name, menu_option):
+        self.window = window
+        self.name = name
+        self.mode = menu_option  # opção do menu
+        self.entity_list: list[Entity] = []
 
     def run(self, ):
         pass
-
